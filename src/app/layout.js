ec564f70ac0eb1c4b8271bc9a996b53405ad4b2c
falcon <falcon_chen@qq.com>
2024-09-10 23:58:17 +0800

    update viewport and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,32 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "CF-Next-DoDo",
+  title: "CF Next ToDo",
   description: "Try develop a todo app with Next.js and Cloudflare, Thanks to v0.dev and Cursor.",
+  icons: {
+    icon: '/icon.ico',
+    apple: '/apple-icon.jpeg',
+  },
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  minimumScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: 'no',
+};
+
+export const appleWebApp = {
+  capable: true,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   );
 }
